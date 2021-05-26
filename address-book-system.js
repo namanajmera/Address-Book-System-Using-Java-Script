2cccc05857ab29f485fb14cab338aa6b5e78ed8d
Naman <cool.naman.ajmera@gmail.com>
2021-05-26 18:19:21 +0530

    Added: Check the validation of contacts feilds.

--- address-book-system.js
+++ address-book-system.js
@@ -11,6 +11,98 @@ class Contact {
     this.email = parameters[7];
   }
 
+  //   Getter of First name
+  get firstName() {
+    return this._firstName;
+  }
+  // Setter of First Name
+  set firstName(firstName) {
+    const FIRST_NAME_REGEX = RegExp("^[A-Z]{1}[a-zA-Z]{2,}");
+    if (FIRST_NAME_REGEX.test(firstName)) this._firstName = firstName;
+    else throw "First Name: " + firstName + " is Invalid";
+  }
+
+  //   Getter of Last name
+  get lastName() {
+    return this._lastName;
+  }
+  // Setter of Last Name
+  set lastName(lastName) {
+    const LAST_NAME_REGEX = RegExp("^[A-Z]{1}[a-zA-Z]{2,}");
+    if (LAST_NAME_REGEX.test(lastName)) this._lastName = lastName;
+    else throw "Last Name: " + lastName + " is Invalid";
+  }
+
+  //   Getter of Address name
+  get address() {
+    return this._address;
+  }
+  // Setter of Address Name
+  set address(address) {
+    const ADDRESS_NAME_REGEX = RegExp("^[a-zA-Z0-9]{4,}");
+    if (ADDRESS_NAME_REGEX.test(address)) this._address = address;
+    else throw "Address Name: " + address + " is Invalid";
+  }
+
+  //   Getter of City name
+  get city() {
+    return this._city;
+  }
+  // Setter of City Name
+  set city(city) {
+    const CITY_NAME_REGEX = RegExp("^[a-zA-Z0-9]{4,}");
+    if (CITY_NAME_REGEX.test(city)) this._city = city;
+    else throw "City Name: " + city + " is Invalid";
+  }
+
+  //   Getter of State name
+  get state() {
+    return this._state;
+  }
+  // Setter of State Name
+  set state(state) {
+    const STATE_NAME_REGEX = RegExp("^[a-zA-Z0-9]{4,}");
+    if (STATE_NAME_REGEX.test(state)) this._state = state;
+    else throw "State Name: " + state + " is Invalid";
+  }
+
+  //   Getter of ZIP
+  get zip() {
+    return this._zip;
+  }
+  // Setter of ZIP
+  set zip(zip) {
+    const ZIP_REGEX = RegExp("^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$");
+    if (ZIP_REGEX.test(zip)) this._zip = zip;
+    else throw "ZIP: " + zip + " is Invalid";
+  }
+
+  //   Getter of Phone Number
+  get phoneNumber() {
+    return this._phoneNumber;
+  }
+  // Setter of Phone Number
+  set phoneNumber(phoneNumber) {
+    const PHONE_NUMBER_REGEX = RegExp(
+      "([0]{1}\\s|[+][9]{1}[1]{1}\\s)?[6-9]{1}[0-9]{9}"
+    );
+    if (PHONE_NUMBER_REGEX.test(phoneNumber)) this._phoneNumber = phoneNumber;
+    else throw "Phone Number : " + phoneNumber + " is Invalid";
+  }
+
+  //   Getter of Email id
+  get email() {
+    return this._email;
+  }
+  // Setter of Email id
+  set email(email) {
+    const EMAIL_ID_REGEX = RegExp(
+      "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z0-9]{2,6}$"
+    );
+    if (EMAIL_ID_REGEX.test(email)) this._email = email;
+    else throw "Email id: " + email + " is Invalid";
+  }
+
   toString() {
     return (
       "First Name: " +
@@ -34,6 +126,7 @@ class Contact {
   }
 }
 
+// UC1 -Create Contact Class
 let contact = new Contact(
   "Naman",
   "Ajmera",
@@ -45,3 +138,49 @@ let contact = new Contact(
   "cool.naman.ajmera@gmail.com"
 );
 console.log(contact.toString());
+
+// UC2: Checking the Valid and Invalid Inputs
+{
+  try {
+    contact.firstName = "Jo";
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    contact.lastName = "Jo";
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    contact.address = "Hom";
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    contact.city = "Jai";
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    contact.state = "Raj";
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    contact.zip = 3039011;
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    contact.phoneNumber = 8696696426;
+  } catch (error) {
+    console.error(error);
+  }
+  try {
+    contact.email = "cool.naman@@gmail.com";
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log("\n Contact After setting Feilds:- \n" + contact.toString());
+}
